82404711cb15ef497adc1b2eca419898e972cfd7
unity <unity@birdtech.dev>
2022-11-29 13:41:22 +0100

    improve formatting

--- src/links.rs
+++ src/links.rs
@@ -1,13 +1,14 @@
 pub struct Socials<'a> {
-    pub social_vec: Vec<(&'a str, &'a str)>
+    pub social_vec: Vec<(&'a str, &'a str)>,
 }
 
 impl Socials<'_> {
-
     pub fn get_link_string(&mut self) -> String {
         self.social_vec.sort();
         let mut str = String::new();
-        self.social_vec.iter().for_each(|link| str.push_str(&format!("{}: {}\n", link.0, link.1)));
-        str 
+        self.social_vec
+            .iter()
+            .for_each(|link| str.push_str(&format!("{}: {}\n", link.0, link.1)));
+        str
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 mod links;
-use std::{env, cmp};
+use std::{cmp, env};
 
 use markov_chain::Chain;
 use rand::prelude::*;
-use reqwest::Error;
 use reqwest::header::USER_AGENT;
+use reqwest::Error;
 use serde::Deserialize;
 use serenity::async_trait;
-use serenity::model::prelude::{Ready};
-use serenity::prelude::*;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::channel::Message;
 use serenity::model::guild::Member;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, CommandResult};
+use serenity::model::prelude::Ready;
+use serenity::prelude::*;
 
 use crate::links::Socials;
 
@@ -32,27 +32,27 @@ struct E621Object {
     fav_count: u32,
 }
 
-#[derive(Deserialize)] 
+#[derive(Deserialize)]
 struct E621Response {
-    posts: Vec<E621Object>
+    posts: Vec<E621Object>,
 }
 
-#[derive(Deserialize)] 
+#[derive(Deserialize)]
 struct E621Tags {
-    general:  Vec<String>,
-    species:  Vec<String>,
-    artist: Vec<String>
+    general: Vec<String>,
+    species: Vec<String>,
+    artist: Vec<String>,
 }
 
 #[derive(Deserialize)]
 struct E621File {
-    url: Option<String>
+    url: Option<String>,
 }
 
 #[derive(Deserialize)]
 struct E621Score {
     down: i32,
-    total:u32
+    total: u32,
 }
 
 #[derive(Deserialize)]
@@ -63,30 +63,49 @@ struct DerpiObject {
     downvotes: i32,
     description: String,
     tags: Vec<String>,
-    view_url: String
+    view_url: String,
 }
 
 #[derive(Deserialize)]
 struct DerpiResponse {
-    images: Vec<DerpiObject>
+    images: Vec<DerpiObject>,
 }
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "h" && !msg.is_own(&ctx) {
-            msg.channel_id.say(&ctx, "h").await.expect("Failed while sending message");
+            msg.channel_id
+                .say(&ctx, "h")
+                .await
+                .expect("Failed while sending message");
         }
     }
 
     async fn guild_member_addition(&self, ctx: Context, new_member: Member) {
-            let guild = new_member.guild_id.to_partial_guild(&ctx)
-            .await.expect("Failed while getting guild");
-            let system_channel_id = guild.system_channel_id.expect("Failed while getting system channel id");
-            guild.channels(&ctx).await.expect("Failed while getting guild channels")
-            .get(&system_channel_id).expect("Failed while getting welcome channel")
-            .say(&ctx, format!("Welcome to the cum zone {}", new_member.nick.expect("Failed while getting username")))
-            .await.expect("Failed while sending welcome message");
+        let guild = new_member
+            .guild_id
+            .to_partial_guild(&ctx)
+            .await
+            .expect("Failed while getting guild");
+        let system_channel_id = guild
+            .system_channel_id
+            .expect("Failed while getting system channel id");
+        guild
+            .channels(&ctx)
+            .await
+            .expect("Failed while getting guild channels")
+            .get(&system_channel_id)
+            .expect("Failed while getting welcome channel")
+            .say(
+                &ctx,
+                format!(
+                    "Welcome to the cum zone {}",
+                    new_member.nick.expect("Failed while getting username")
+                ),
+            )
+            .await
+            .expect("Failed while sending welcome message");
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
@@ -101,7 +120,10 @@ async fn main() {
         .group(&GENERAL_GROUP);
 
     let token = env::var("DISCORD_TOKEN").expect("token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILDS | GatewayIntents::GUILD_MEMBERS;
+    let intents = GatewayIntents::non_privileged()
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILDS
+        | GatewayIntents::GUILD_MEMBERS;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
@@ -115,7 +137,8 @@ async fn main() {
 
 #[command]
 async fn fursona(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Check out my fursona here: https://ref.birdtech.dev").await?;
+    msg.reply(ctx, "Check out my fursona here: https://ref.birdtech.dev")
+        .await?;
     Ok(())
 }
 
@@ -123,91 +146,181 @@ async fn fursona(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 async fn markov(ctx: &Context, msg: &Message) -> CommandResult {
     let mut generated_text = String::new();
-    let mut chain:Chain<&str> = Chain::new(3);
+    let mut chain: Chain<&str> = Chain::new(3);
     chain.train(vec![]);
-    chain.generate().iter().for_each(|generated| generated_text.push_str(&format!("{} ", generated)));
-    msg.reply(&ctx, generated_text).await.expect("Couldn't send message");
+    chain
+        .generate()
+        .iter()
+        .for_each(|generated| generated_text.push_str(&format!("{} ", generated)));
+    msg.reply(&ctx, generated_text)
+        .await
+        .expect("Couldn't send message");
     Ok(())
 }
 
 #[command]
-async fn socials (ctx: &Context, msg: &Message) -> CommandResult {
-    let mut socials = Socials { social_vec: vec![
-        ("Mastodon", "https://tech.lgbt/@bird"), 
-        ("Twitter", "https://twitter.com/unitybirb"),
-        ("Tumblr",  "https://unity-birdposts.tumblr.com"),
-        ("Cohost", "https://cohost.org/unitybirb")
-    ]};
+async fn socials(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut socials = Socials {
+        social_vec: vec![
+            ("Mastodon", "https://tech.lgbt/@bird"),
+            ("Twitter", "https://twitter.com/unitybirb"),
+            ("Tumblr", "https://unity-birdposts.tumblr.com"),
+            ("Cohost", "https://cohost.org/unitybirb"),
+        ],
+    };
     msg.reply(ctx, socials.get_link_string()).await?;
     Ok(())
 }
 
 #[command]
-async fn stream (ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "My stream can be found at https://www.twitch.tv/unitybirb").await?;
+async fn stream(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(
+        ctx,
+        "My stream can be found at https://www.twitch.tv/unitybirb",
+    )
+    .await?;
     Ok(())
 }
 
 #[command]
-async fn logs (ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "https://www.fflogs.com/character/id/13439791").await?;
+async fn logs(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "https://www.fflogs.com/character/id/13439791")
+        .await?;
     Ok(())
 }
 
 #[command]
-async fn derpi (ctx: &Context, msg: &Message) -> CommandResult {
+async fn derpi(ctx: &Context, msg: &Message) -> CommandResult {
     let client = reqwest::Client::new();
     let querystring = &msg.content[6..];
-    let url = format!("https://derpibooru.org/api/v1/json/search/images?q={query}&filter_id={filter}", query = &querystring, filter = 37432);
-    let response = client.get(&url).header(USER_AGENT, "Birdbrain").send().await.expect("Couldn't get response");
+    let url = format!(
+        "https://derpibooru.org/api/v1/json/search/images?q={query}&filter_id={filter}",
+        query = &querystring,
+        filter = 37432
+    );
+    let response = client
+        .get(&url)
+        .header(USER_AGENT, "Birdbrain")
+        .send()
+        .await
+        .expect("Couldn't get response");
     let derpi_object_list: Result<DerpiResponse, Error> = response.json().await;
     let deserialized = match &derpi_object_list {
         Ok(object_list) => match object_list.images.len() {
             0 => {
-                msg.reply(&ctx, "Couldn't find any images! Are you sure the tags you're looking for exist?").await.expect("Couldn't send error message");
-                return Ok(())
+                msg.reply(
+                    &ctx,
+                    "Couldn't find any images! Are you sure the tags you're looking for exist?",
+                )
+                .await
+                .expect("Couldn't send error message");
+                return Ok(());
             }
             _ => &object_list.images,
-        }
-        Err(error) =>  panic!("Deserialization failed with error {}", &error)
+        },
+        Err(error) => panic!("Deserialization failed with error {}", &error),
     };
-    let received_picture = &deserialized.get(get_random_number(deserialized.len())).expect("Couldn't get image");
-    let artist = &received_picture.tags.iter().find(|x| x.starts_with("artist:")).expect("Couldn't extract artist")[7..];
-    msg.reply(ctx, format!("Found image {} by {}. Its score is {} with {} downvotes and {} faves.\n{}",
-        &received_picture.id, &artist, &received_picture.score, &received_picture.downvotes, &received_picture.faves, &received_picture.view_url)).await.expect("Couldn't post image");
-    msg.reply(ctx, format!("Description: {}", &received_picture.description[..get_description_max(&received_picture.description)])).await.expect("Couldn't post message");
+    let received_picture = &deserialized
+        .get(get_random_number(deserialized.len()))
+        .expect("Couldn't get image");
+    let artist = &received_picture
+        .tags
+        .iter()
+        .find(|x| x.starts_with("artist:"))
+        .expect("Couldn't extract artist")[7..];
+    msg.reply(
+        ctx,
+        format!(
+            "Found image {} by {}. Its score is {} with {} downvotes and {} faves.\n{}",
+            &received_picture.id,
+            &artist,
+            &received_picture.score,
+            &received_picture.downvotes,
+            &received_picture.faves,
+            &received_picture.view_url
+        ),
+    )
+    .await
+    .expect("Couldn't post image");
+    msg.reply(
+        ctx,
+        format!(
+            "Description: {}",
+            &received_picture.description[..get_description_max(&received_picture.description)]
+        ),
+    )
+    .await
+    .expect("Couldn't post message");
     Ok(())
 }
 
 #[command]
-async fn e621 (ctx: &Context, msg: &Message) -> CommandResult {
+async fn e621(ctx: &Context, msg: &Message) -> CommandResult {
     let client = reqwest::Client::new();
     let querystring = &msg.content[5..];
-    let url = format!("https://e621.net/posts.json?tags={query}", query = &querystring);
-    let response = client.get(&url).header(USER_AGENT, "Birdbrain").send().await.expect("Couldn't get response");
+    let url = format!(
+        "https://e621.net/posts.json?tags={query}",
+        query = &querystring
+    );
+    let response = client
+        .get(&url)
+        .header(USER_AGENT, "Birdbrain")
+        .send()
+        .await
+        .expect("Couldn't get response");
     let e621_object_list: Result<E621Response, Error> = response.json().await;
     let deserialized = match &e621_object_list {
         Ok(object_list) => match object_list.posts.len() {
             0 => {
-                msg.reply(&ctx, "Couldn't find any images! Are you sure the tags you're looking for exist?").await.expect("Couldn't send error message");
-                return Ok(())
+                msg.reply(
+                    &ctx,
+                    "Couldn't find any images! Are you sure the tags you're looking for exist?",
+                )
+                .await
+                .expect("Couldn't send error message");
+                return Ok(());
             }
             _ => &object_list.posts,
-        }
-        Err(error) =>  panic!("Deserialization failed with error {}", &error)
+        },
+        Err(error) => panic!("Deserialization failed with error {}", &error),
     };
-    let filtered: Vec<&E621Object> = deserialized.iter().filter(|post| post.file.url.is_some()).collect();
-    let received_picture = filtered.get(get_random_number(deserialized.len())).expect("Couldn't get image");
+    let filtered: Vec<&E621Object> = deserialized
+        .iter()
+        .filter(|post| post.file.url.is_some())
+        .collect();
+    let received_picture = filtered
+        .get(get_random_number(deserialized.len()))
+        .expect("Couldn't get image");
     let artists = &received_picture.tags.artist;
     let artist = match &artists.len() {
         0 => String::from("unknown artist"),
-        _ => artists.get(0).unwrap().to_owned()
+        _ => artists.get(0).unwrap().to_owned(),
     };
     let description = &received_picture.description;
     let url = received_picture.file.url.as_ref().unwrap();
-    msg.reply(ctx, format!("Found image {} by {}. Its score is {} with {} downvotes and {} faves.\n{}",
-        &received_picture.id, artist,  &received_picture.score.total, &received_picture.score.down, &received_picture.fav_count, url)).await.expect("Couldn't post image");
-    msg.reply(ctx, format!("Description: {}", &description[..get_description_max(description)])).await.expect("Couldn't post message");
+    msg.reply(
+        ctx,
+        format!(
+            "Found image {} by {}. Its score is {} with {} downvotes and {} faves.\n{}",
+            &received_picture.id,
+            artist,
+            &received_picture.score.total,
+            &received_picture.score.down,
+            &received_picture.fav_count,
+            url
+        ),
+    )
+    .await
+    .expect("Couldn't post image");
+    msg.reply(
+        ctx,
+        format!(
+            "Description: {}",
+            &description[..get_description_max(description)]
+        ),
+    )
+    .await
+    .expect("Couldn't post message");
     Ok(())
 }
 
@@ -222,12 +335,14 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
         "derpi" => "Queries Derpibooru with your given tags. Usage: !derpi [tag1], [tag2]",
         _ => "The following commands are available: !socials, !fursona, !stream, !logs, !e621 and !derpi"
     };
-    msg.reply(ctx, response).await.expect("Cound't post message");
+    msg.reply(ctx, response)
+        .await
+        .expect("Cound't post message");
     Ok(())
 }
 
 fn get_random_number(max: usize) -> usize {
-   rand::thread_rng().gen_range(0..max)
+    rand::thread_rng().gen_range(0..max)
 }
 
 fn get_description_max(description: &str) -> usize {
